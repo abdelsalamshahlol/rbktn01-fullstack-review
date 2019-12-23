a5826fe1258c5cb0bbd82083e637d10184e5026c
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-23 14:05:52 +0100

    schema done

--- database/index.js
+++ database/index.js
@@ -1,8 +1,40 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+let ownerSchema = mongoose.Schema({
+  "login": String,
+  "id": Number,
+  "avatar_url": String,
+  "gravatar_id": String,
+  "type": String
+});
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  "id": Number,
+  "name": String,
+  "full_name": String,
+  "owner": ownerSchema,
+  "private": Boolean,
+  "html_url": String,
+  "description": String,
+  "fork": Boolean,
+  "created_at": Date,
+  "updated_at": Date,
+  "pushed_at": Date,
+  "git_url": String,
+  "clone_url": String,
+  "stargazers_count": Number,
+  "watchers_count": Number,
+  "language": String,
+  "has_issues": Boolean,
+  "has_downloads": Boolean,
+  "has_wiki": Boolean,
+  "has_pages": Boolean,
+  "mirror_url": String,
+  "forks": Number,
+  "open_issues": Number,
+  "watchers": Number,
+  "default_branch": String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
